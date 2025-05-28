212ddb7dc3a3e137854963bf94ed9d7aa212982e
Shuhrat Shokirov <ga26099111@gmail.com>
2025-05-28 20:47:13 +0500

    Initial commit

--- config/settings.py
+++ config/settings.py
@@ -17,6 +17,11 @@ from pathlib import Path
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 
+DEBUG = True
+
+ALLOWED_HOSTS = ['*']
+
+
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
